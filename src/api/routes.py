748ef9ce53537f4afb11a15ca5c22b167a12bfc3
Fernando R. G. <nando.rodriguez.gomez89@gmail.com>
2025-07-17 16:35:14 +0000

    endpoint get huts created

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,10 +4,12 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from api.models import db, Users, Huts
 
 api = Blueprint('api', __name__)
 CORS(api)  # Allow CORS requests to this API
+
+host = 'https://friendly-guide-g4v6q796wprf9q67-3001.app.github.dev/admin/'
 
 
 @api.route('/hello', methods=['GET'])
@@ -15,3 +17,18 @@ def handle_hello():
     response_body = {}
     response_body['message'] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     return response_body, 200
+
+
+@api.route('/huts', methods=['GET'])
+def get_huts():
+    response_body = {}
+    response_body['message'] = "Las cabañas se han cargado correctamente."
+    rows = db.session.execute(db.select(Huts)).scalars()
+    response_body['result'] = [rows.serialize() for row in rows]
+    return response_body, 200
+
+
+@api.route('/huts/<int:huts.id>', methods=['POST'])
+def post_huts():
+    pass
+    
