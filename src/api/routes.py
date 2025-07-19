e3c1d1df000213bc8a70f6f358f0e9e090ac7dde
Fernando R. G. <nando.rodriguez.gomez89@gmail.com>
2025-07-19 02:05:05 +0000

    [routes] create: huts endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -122,11 +122,140 @@ def user(id):
     
    
 @api.route('/huts', methods=['GET'])
+@jwt_required()
 def get_huts():
     response_body = {}
     response_body['message'] = "Las cabañas se han cargado correctamente."
     rows = db.session.execute(db.select(Huts)).scalars()
-    response_body['result'] = [rows.serialize() for row in rows]
+    response_body['results'] = [row.serialize() for row in rows]
     return response_body, 200
 
 
+@api.route('/huts', methods=['POST'])
+@jwt_required() 
+def post_huts():
+    claims = get_jwt()
+    response_body = {}
+    if not claims.get('is_admin', False):
+        response_body['message'] = 'El usuario no tiene permiso para añadir cabañas.'
+        return response_body, 403
+    data = request.json
+    response_body = {}
+    hut = Huts()
+    hut.name = data.get('name', hut.name)
+    hut.description = data.get('description', hut.description)
+    hut.capacity = data.get('capacity', hut.capacity)
+    hut.bedrooms = data.get('bedrooms', hut.bedrooms)
+    hut.bathroom = data.get('bathroom', hut.bathroom)
+    hut.price_per_night = data.get('price_per_night', hut.price_per_night)
+    hut.location_id = data.get('location_id', hut.location_id)
+    hut.is_active = data.get('is_active', hut.is_active)
+    db.session.add(hut)
+    db.session.commit()
+    response_body['message'] = 'La cabaña se ha añadido correctamente.'
+    response_body['results'] = hut.serialize()
+    return response_body, 201
+
+
+@api.route('/huts/<int:id>', methods=['PUT'])
+@jwt_required()
+def put_huts(id):
+    response_body = {}
+    data = request.json
+    claims = get_jwt()
+    if not claims.get('is_admin', False):
+        response_body['message'] = "Se necesita permiso del administrador."
+        return response_body, 403
+    hut = db.session.get(Huts, id)
+    if not hut:
+        response_body['message'] = "Cabaña no encontrada."
+        return response_body, 404
+    if 'name' in data and data['name'] != hut.name:
+        existing_name = db.session.execute(db.select(Huts).where(Huts.name == data['name'])).scalar()
+        if existing_name:
+            response_body['message'] = "El nombre ya existe."
+            return response_body, 409
+    hut.name = data.get('name', hut.name)
+    hut.description = data.get('description', hut.description)
+    hut.capacity = data.get('capacity', hut.capacity)
+    hut.bedrooms = data.get('bedrooms', hut.bedrooms)
+    hut.bathroom = data.get('bathroom', hut.bathroom)
+    hut.price_per_night = data.get('price_per_night', hut.price_per_night)
+    hut.location_id = data.get('location_id', hut.location_id)
+    hut.is_active = data.get('is_active', hut.is_active)
+    db.session.commit()
+    response_body['message'] = 'Los datos de la cabaña se han actualizado correctamente.'
+    response_body['results'] = hut.serialize()
+    return response_body, 200
+
+
+@api.route('/huts/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_hut(id):
+    response_body = {}
+    claims = get_jwt()
+    if not claims.get('is_admin', False):
+        response_body['message'] = 'Se requiere permiso de administrador'
+        return response_body, 403
+    hut = db.session.get(Huts, id)
+    if not hut:
+        response_body['message'] = f'La cabaña con ID {id} no existe.'
+        return response_body, 404
+    db.session.execute(db.delete(Huts_album).where(Huts_album.hut_id == id))
+    db.session.delete(hut)
+    db.session.commit()
+    response_body['message'] = f'La cabaña {hut.name} se ha eliminado correctamente.'
+    return response_body, 200
+
+
+@api.route('/huts_album', methods=['GET'])
+@jwt_required()
+def get_huts_album():
+    response_body = {}
+    response_body['message'] = "Las fotografías de las cabañas se han cargado satisfactoriamente."
+    rows = db.session.execute(db.select(Huts_album)).scalars()
+    response_body['results'] = [row.serialize() for row in rows]
+    return response_body, 200
+
+
+@api.route('/huts_album/<int:id>', methods=['GET'])
+@jwt_required()
+def get_current_hut_album(id):
+    response_body = {}
+    if not db.session.get(Huts, id):
+        response_body['message'] = "La cabaña no existe."
+        return response_body, 404
+    response_body['message'] = "Las fotografías de las cabañas se han cargado satisfactoriamente."
+    rows = db.session.execute(db.select(Huts_album).where(Huts_album.hut_id == id)).scalars()
+    response_body['results'] = [row.serialize() for row in rows]
+    return response_body, 200
+
+
+@api.route('/huts_album', methods=['POST'])
+@jwt_required()
+def post_huts_album():
+    response_body = {}
+    data = request.json
+    hut_id = data.get('hut_id')
+    valid_types = ["bedroom", "bathroom", "living_room", "kitchen", "other_picture"]
+    claims = get_jwt()
+    if not claims.get('is_admin', False):
+        response_body['message'] = "El usuario no tiene permiso para añadir fotografías."
+        return response_body, 403
+    if not hut_id:
+        return {"message": "Se requiere hut_id"}, 400
+    if 'type' in data and data['type'] not in valid_types:
+        response_body['message'] = "Tipo no válido."
+        return response_body, 400
+    if not db.session.get(Huts, hut_id):
+        return {"message": "La cabaña no existe"}, 404
+    hut_album = Huts_album(
+        hut_id=hut_id,
+        type=data.get('type'),
+        image_url=data['image_url']
+    )
+    db.session.add(hut_album)
+    db.session.commit()
+    response_body['message'] = "Las fotografías se han añadido satisfactoriamente."
+    response_body['results'] = hut_album.serialize()
+    return response_body, 201
